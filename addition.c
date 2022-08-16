f50f4e5cc3b8f0a90da25e1dabbffbd15b77a7af
Neeraj-Joshi4454 <neerajmjoshi1@gmail.com>
2022-08-16 14:58:53 +0530

    first commit

---
+++ addition.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    int a = 45;
+    int b = 89;
+
+    int add = a + b;
+
+    int mul = a * b; 
+ 
+    
+    printf("addition of two number = %d \n" , add);
+    
+    printf("multiplication of two numbers = %d\n" , mul);
+
+
+
+    return 1;
+}
